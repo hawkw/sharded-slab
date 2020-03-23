8c5b334ba5ffa254a31fa8455ba171ffebee78fd
Bhargav Voleti <voletibv@amazon.com>
2020-03-23 13:58:07 -0700

    Fixed test code

--- src/pool_tests.rs
+++ src/pool_tests.rs
@@ -8,43 +8,58 @@ use loom::{
 };
 
 #[derive(Default, Debug)]
-struct DontDropMe {
+struct State {
+    is_dropped: AtomicBool,
+    is_cleared: AtomicBool,
     id: usize,
-    drop: AtomicBool,
-    clear: AtomicBool,
+}
+
+impl PartialEq for State {
+    fn eq(&self, other: &State) -> bool {
+        self.id.eq(&other.id)
+    }
+}
+
+#[derive(Clone, Default, Debug)]
+struct DontDropMe(Arc<State>);
+
+impl PartialEq for DontDropMe {
+    fn eq(&self, other: &DontDropMe) -> bool {
+        self.0.eq(&other.0)
+    }
 }
 
 impl DontDropMe {
-    fn new(id: usize) -> Self {
-        Self {
+    fn new(id: usize) -> (Arc<State>, Self) {
+        let state = Arc::new(State {
+            is_dropped: AtomicBool::new(false),
+            is_cleared: AtomicBool::new(false),
             id,
-            drop: AtomicBool::new(false),
-            clear: AtomicBool::new(false),
-        }
+        });
+        (state.clone(), Self(state))
     }
 }
 
 impl Drop for DontDropMe {
     fn drop(&mut self) {
-        test_println!("-> DontDropMe drop: dropping data {:?}", self.id);
-        self.drop.store(true, Ordering::SeqCst);
+        test_println!("-> DontDropMe drop: dropping data {:?}", self.0.id);
+        self.0.is_dropped.store(true, Ordering::SeqCst)
     }
 }
 
-impl Clear for Arc<DontDropMe> {
+impl Clear for DontDropMe {
     fn clear(&mut self) {
-        test_println!("-> DontDropMe clear: clearing data {:?}", self.id);
-        self.clear.store(true, Ordering::SeqCst);
+        test_println!("-> DontDropMe clear: clearing data {:?}", self.0.id);
+        self.0.is_cleared.store(true, Ordering::SeqCst);
     }
 }
 
 #[test]
 fn dont_drop() {
     run_model("dont_drop", || {
-        let pool: Pool<Arc<DontDropMe>> = Pool::new();
-        let item1 = Arc::new(DontDropMe::new(1));
+        let pool: Pool<DontDropMe> = Pool::new();
+        let (item1, value) = DontDropMe::new(1);
         test_println!("-> dont_drop: Inserting into pool {}", item1.id);
-        let value = item1.clone();
         let idx = pool
             .create(move |item| *item = value.clone())
             .expect("Create");
@@ -52,36 +67,34 @@ fn dont_drop() {
         test_println!("-> dont_drop: clearing idx: {}", idx);
         pool.clear(idx);
 
-        assert!(!item1.drop.load(Ordering::SeqCst));
-        assert!(item1.clear.load(Ordering::SeqCst));
+        assert!(!item1.is_dropped.load(Ordering::SeqCst));
+        assert!(item1.is_cleared.load(Ordering::SeqCst));
     });
 }
 
 #[test]
 fn dont_drop_across_threads() {
     run_model("dont_drop_across_threads", || {
-        let pool: Arc<Pool<Arc<DontDropMe>>> = Arc::new(Pool::new());
+        let pool: Arc<Pool<DontDropMe>> = Arc::new(Pool::new());
 
-        let item1 = Arc::new(DontDropMe::new(1));
-        let value = item1.clone();
+        let (item1, value) = DontDropMe::new(1);
         let idx1 = pool
             .create(move |item| *item = value.clone())
             .expect("Create");
 
         let p = pool.clone();
-        let item = item1.clone();
+        let test_value = item1.clone();
         let t1 = thread::spawn(move || {
-            assert_eq!(p.get(idx1).unwrap().id, item.id);
+            assert_eq!(p.get(idx1).unwrap().0.id, test_value.id);
         });
 
-        let item = item1.clone();
-        assert!(item.drop.load(Ordering::SeqCst));
-        assert!(item.clear.load(Ordering::SeqCst));
+        assert!(!item1.is_dropped.load(Ordering::SeqCst));
+        assert!(item1.is_cleared.load(Ordering::SeqCst));
 
         t1.join().expect("thread 1 unable to join");
         pool.clear(idx1);
 
-        assert!(!item1.drop.load(Ordering::SeqCst));
-        assert!(item1.clear.load(Ordering::SeqCst));
+        assert!(!item1.is_dropped.load(Ordering::SeqCst));
+        assert!(item1.is_cleared.load(Ordering::SeqCst));
     })
 }
