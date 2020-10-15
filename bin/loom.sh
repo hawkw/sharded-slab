89e01cf5cd16170e8bfde037a93a6593b9772a5d
Eliza Weisman <eliza@buoyant.io>
2020-10-15 12:24:19 -0700

    don't run doctests with loom
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- bin/loom.sh
+++ bin/loom.sh
@@ -3,10 +3,10 @@
 #
 # The nightly Rust compiler is used to enable Loom's location tracking support.
 # This allows Loom to emit diagnostics that include the caller's source code
-# locations when errors occur. 
+# locations when errors occur.
 #
 # The tests are compiled in release mode to improve performance, but debug
-# assertions are enabled. 
+# assertions are enabled.
 #
 # Any arguments to this script are passed to the `cargo test` invocation.
 
@@ -15,4 +15,4 @@ RUSTFLAGS="${RUSTFLAGS} --cfg loom --cfg loom_nightly -C debug-assertions=on" \
     LOOM_CHECKPOINT_INTERVAL="${LOOM_CHECKPOINT_INTERVAL:-1}" \
     LOOM_LOG=1 \
     LOOM_LOCATION=1 \
-    cargo +nightly test --release "$@"
+    cargo +nightly test --release --lib "$@"
