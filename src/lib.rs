fdc52d90f28fe962675f5ebad011fdb63031b785
Eliza Weisman <eliza@buoyant.io>
2021-08-01 09:00:16 -0700

    chore: move macros to macro module
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/lib.rs
+++ src/lib.rs
@@ -202,31 +202,8 @@
 #![doc(html_root_url = "https://docs.rs/sharded-slab/0.1.1")]
 #![warn(missing_debug_implementations, missing_docs, missing_doc_code_examples)]
 
-macro_rules! test_println {
-    ($($arg:tt)*) => {
-        if cfg!(test) && cfg!(slab_print) {
-            if std::thread::panicking() {
-                // getting the thread ID while panicking doesn't seem to play super nicely with loom's
-                // mock lazy_static...
-                println!("[PANIC {:>17}:{:<3}] {}", file!(), line!(), format_args!($($arg)*))
-            } else {
-                println!("[{:?} {:>17}:{:<3}] {}", crate::Tid::<crate::DefaultConfig>::current(), file!(), line!(), format_args!($($arg)*))
-            }
-        }
-    }
-}
-
-#[cfg(all(test, loom))]
-macro_rules! test_dbg {
-    ($e:expr) => {
-        match $e {
-            e => {
-                test_println!("{} = {:?}", stringify!($e), &e);
-                e
-            }
-        }
-    };
-}
+#[macro_use]
+mod macros;
 
 pub mod implementation;
 pub mod pool;
