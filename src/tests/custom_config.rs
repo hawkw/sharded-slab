0efa42ff6101fd23186442ccd2ce64a1088c06e6
Eliza Weisman <eliza@buoyant.io>
2023-09-27 11:42:30 -0700

    chore(CI): use musl for 32-bit builds (#86)
    This branch changes the 32-bit CI builds to use a musl toolchain rather
    than glibc, so that CI can stop complaining that there's no 32-bit
    glibc.
    
    Additionally, I fixed some compilation errors in the tests on 32-bit
    systems, which were merged accidentally because of automerge being
    configured slightly wrongly.

--- src/tests/custom_config.rs
+++ src/tests/custom_config.rs
@@ -16,9 +16,9 @@ impl Config for CustomConfig {
 }
 
 #[cfg(not(target_pointer_width = "64"))]
-impl Config for CustomConfig32 {
+impl Config for CustomConfig {
     const INITIAL_PAGE_SIZE: usize = 16;
-    const MAX_PAGES: usize = 7;
+    const MAX_PAGES: usize = 6;
     const MAX_THREADS: usize = 128;
     const RESERVED_BITS: usize = 12;
 }
