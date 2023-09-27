0d8cd701576048c0018c047503e6fb9d5b9c9ccd
Eliza Weisman <eliza@buoyant.io>
2023-09-27 10:50:13 -0700

    fix: use a smaller `CustomConfig` for 32-bit tests (#84)
    This way, the custom-config tests from PR #80 can still be run on 32-bit
    targets.
    
    Fixes #82

--- src/tests/custom_config.rs
+++ src/tests/custom_config.rs
@@ -3,14 +3,25 @@
 
 #![cfg(not(loom))]
 
-use crate::{Config, Slab};
+use crate::{cfg::CfgPrivate, Config, Slab};
 
 struct CustomConfig;
+
+#[cfg(target_pointer_width = "64")]
 impl Config for CustomConfig {
     const INITIAL_PAGE_SIZE: usize = 32;
     const MAX_PAGES: usize = 15;
     const MAX_THREADS: usize = 256;
     const RESERVED_BITS: usize = 24;
+}
+
+// Use a smaller config on 32-bit targets.
+#[cfg(not(target_pointer_width = "64"))]
+impl Config for CustomConfig {
+    const INITIAL_PAGE_SIZE: usize = 16;
+    const MAX_PAGES: usize = 6;
+    const MAX_THREADS: usize = 128;
+    const RESERVED_BITS: usize = 12;
 }
 
 // We should repeat actions several times to detect invalid lifecycle changes.
@@ -29,6 +40,8 @@ fn slab_eq(mut lhs: Slab<u64, impl Config>, mut rhs: Slab<u64, impl Config>) {
 /// Initially, it revealed bugs in the `Slot::release_with()` implementation.
 #[test]
 fn insert_remove() {
+    eprintln!("bits={}; config={:#?}", usize::BITS, CustomConfig::debug());
+
     let default_slab = Slab::<u64, _>::new();
     let custom_slab = Slab::<u64, _>::new_with_config::<CustomConfig>();
 
@@ -47,6 +60,8 @@ fn insert_remove() {
 /// Initially, it revealed bugs in the `Slot::get()` implementation.
 #[test]
 fn double_get() {
+    eprintln!("bits={}; config={:#?}", usize::BITS, CustomConfig::debug());
+
     let default_slab = Slab::<u64, _>::new();
     let custom_slab = Slab::<u64, _>::new_with_config::<CustomConfig>();
 
